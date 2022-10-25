fe53475d8189141c88074e8957014edb94ef7d87
Edfram-Guerra <edfram.guerra@gmail.com>
2022-10-25 16:34:18 -0300

    Exercicio resolvido

--- 03/index.js
+++ 03/index.js
@@ -1,1 +1,15 @@
+// Num determinado jogo, cada rodada dura 10 segundos. 
+// Faça um programa que imprima o nome de cada jogador em seu devido momento.
+// Ao final da rodada, o programa deverá imprimir a mensagem: A rodada terminou
+
 const jogadores = ["Guido", "Dani", "Ruli", "Diego", "Vidal"];
+const tempo = 10000 / jogadores.length;
+
+
+const jogo = setInterval(() => {
+    for (let item of jogadores) {
+        console.log(item)
+    }
+    console.log("A rodada terminou!")
+    clearInterval(jogo);
+}, tempo);
