7ef07cf76b217cc7bdbf876605174bae3800de5e
Edfram-Guerra <edfram.guerra@gmail.com>
2022-10-27 13:28:57 -0300

    Exercicio resolvido

--- 04/index.js
+++ 04/index.js
@@ -1,15 +1,3 @@
-// Desenvolva um programa que simula um timer simples.
-
-// Logo após iniciar, o seu programa deverá imprimir na tela uma mensagem de início do timer:
-//  "Timer iniciado, disparando alarme em X segundos!".
-// Note que X corresponde ao tempo em que o seu programa irá aguardar para enfim despertar.
-
-// O seu programa deverá aguardar o tempo em segundos especificado na variável tempoParaAlarmarEmSegundos,
-//  só então irá despertar mostrando repetidamente a mensagem "Beep beep!", uma mensagem a cada segundo, até
-//  que o tempo da variável tempoAlarmandoEmSegundos se esgote.
-
-// Por fim, o programa é finalizado.
-
 let tempoParaAlarmarEmSegundos = 10;
 let tempoAlarmandoEmSegundos = 5;
 let contador = 0;
